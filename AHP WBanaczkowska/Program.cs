c0941d1c9bf4df230dd7c44d82005ede619121ba
Vintrdottir <b.weronika93@gmail.com>
2020-07-08 19:15:39 +0200

    cleaning imports

--- AHP WBanaczkowska/Program.cs
+++ AHP WBanaczkowska/Program.cs
@@ -1,17 +1,11 @@
-﻿using AHP;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace AHP
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
